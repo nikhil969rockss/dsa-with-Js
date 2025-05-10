d59f7810c0ec9fa938a238dd43d546adb4652ad9
Nikhil Randhawa <ranveersingh@Ranveers-MacBook.local>
2025-05-10 16:28:24 +0530

    Queue front, rear, isEmpty methods

--- src/Array/Queue.js
+++ src/Array/Queue.js
@@ -1,51 +1,61 @@
-const prompt = require('prompt-sync')()
+const prompt = require("prompt-sync")();
 
 // Queue is like [10, 20, 30] which based on principle FIFO like in real world people queue works whosoever comes first has to go first(FIRST IN FIRST OUT) so 10 entered first 10 will be the first which leave first
 
-
 // enqueue = entering the element
 // dequeue = removing the element
-let arr = []
-let currentSize = arr.length
+let arr = [];
+let currentSize = arr.length;
 let maxSize = 5;
 
-function enqueue(newValue){
-  if (currentSize >= maxSize){
+function enqueue(newValue) {
+  if (currentSize >= maxSize) {
     console.log("Queue is already full");
     return console.log(arr);
-  }else {
-    arr[currentSize] = newValue
-    currentSize++
+  } else {
+    arr[currentSize] = newValue;
+    currentSize++;
     return console.log(arr);
   }
-
 }
-function dequeue(){
+function dequeue() {
   // has to delete the first element in the queue
-// right element to left at the end removing the last element hence we require the desire output
-  if (currentSize > 0){
-    for (let i =0; i<arr.length; i++){
-      arr [i] = arr[i+1]
-
+  // right element to left at the end removing the last element hence we require the desire output
+  if (currentSize > 0) {
+    for (let i = 0; i < arr.length; i++) {
+      arr[i] = arr[i + 1];
     }
-    currentSize--
-    arr.length = currentSize
-
-  }else {
-    console.log('Queue is already empty ');
-    
+    currentSize--;
+    arr.length = currentSize;
+  } else {
+    console.log("Queue is already empty ");
   }
-  
-  return console.log(arr);
 
+  return console.log(arr);
 }
 
-
-enqueue(70); 
+enqueue(70);
 enqueue(30);
 enqueue(40);
 enqueue(50);
 enqueue(60);
 enqueue(10); // cannot enqueue further
-dequeue() // removes first element 70
+dequeue(); // removes first element 70
 
+
+// Can make 3 other function
+
+function rear(){
+// which return the rear element of the queue
+// by arr[currentSize -1] // rear element
+}
+function front() {
+  // which return the front element of the queue 
+  // by arr[0] // front element
+
+}
+function isEmpty(){
+  // which returns a boolean, check if the
+  // currentSize <= 0 return true else false
+
+}
